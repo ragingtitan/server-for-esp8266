ff88495eab384ae489dd5b6d8190826cdbb1e20c
Anish Das <135655078+ragingtitan@users.noreply.github.com>
2024-07-27 06:09:42 +0000

    changed index.js

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.get('/', (req, res) => {
   res.send('Hello from the server!');
 });
 
-app.listen(port, '0.0.0.0', () => {
+console.log(port )
+app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
