2f94c995b1d8460a57bce1e3c75c459550c7b3f1
Anish Das <135655078+ragingtitan@users.noreply.github.com>
2024-07-28 08:37:57 +0000

    chanf

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
-const { exec } = require('child_process');
 const express = require('express');
+const NodeWebcam = require('node-webcam');
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
+const PORT = 8000;
 
-app.get('/', (req, res) => {
-  exec('python Video.py', (error, stdout, stderr) => {
-    console.log('Executing script...');
-    console.log('Error:', error);
-    console.log('Stdout:', stdout);
-    console.log('Stderr:', stderr);
-    
-    if (error) {
-      res.status(500).send(`Error executing script: ${error.message}`);
-      return;
+const videoCapture = (callback) => {
+  const opts = {
+    width: 1366,
+    height: 768,
+    quality: 100,
+    delay: 0,
+    saveShots: true,
+    output: "jpeg",
+    device: false,
+    callbackReturn: "location",
+    verbose: false
+  };
+
+  // Create webcam instance
+  const Webcam = NodeWebcam.create(opts);
+
+  // Generate a sanitized filename
+  const generateFilename = () => {
+    const now = new Date();
+    const timestamp = now.toISOString().replace(/:/g, '-').replace(/\./g, '-');
+    return `test_picture_${timestamp}`;
+  };
+
+  const filename = generateFilename();
+
+  // Capture an image
+  Webcam.capture(filename, function(err, data) {
+    if (err) {
+      console.error("Error capturing image: ", err);
+      callback(err, null);
+    } else {
+      console.log("Image captured and saved at: ", data);
+      callback(null, data);
     }
-    if (stderr) {
-      res.status(500).send(`Script stderr: ${stderr}`);
-      return;
+  });
+};
+
+app.get('/', async (req, res) => {
+  videoCapture((err, data) => {
+    if (err) {
+      res.status(500).send("Error capturing image");
+    } else {
+      res.status(200).send(`Image captured and saved at: ${data}`);
     }
-    res.send(`Script executed successfully! Output: ${stdout}`);
   });
 });
 
-app.listen(80, () => {
-  console.log('Server listening on port 3000!');
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}!`);
 });
